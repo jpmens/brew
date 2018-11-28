1e261da86fa77dbc9908c506a79fb7deb7c04575
Jan-Piet Mens <jp@mens.de>
2018-11-28 17:10:49 +0100

    bump revgeod 0.1.8

--- Formula/revgeod.rb
+++ Formula/revgeod.rb
@@ -1,8 +1,8 @@
 class Revgeod < Formula
   desc "A reverse Geo lookup service written in C, accessible via HTTP and backed by OpenCage and LMDB"
   homepage "https://github.com/jpmens/revgeod/"
-  url "https://github.com/jpmens/revgeod/archive/0.1.7.tar.gz"
-  sha256 "3c23fdf57d6a45f049c8dbb90c01957bab9b65d3832bcbf5095401cbbe8a973e"
+  url "https://github.com/jpmens/revgeod/archive/0.1.8.tar.gz"
+  # sha256 "3c23fdf57d6a45f049c8dbb90c01957bab9b65d3832bcbf5095401cbbe8a973e"
 
   depends_on "libmicrohttpd"
   depends_on "lmdb"
